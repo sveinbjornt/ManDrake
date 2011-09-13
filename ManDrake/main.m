f0c27402ec0d1d1dc7de8af8954a67bd5eca38f6
Sveinbjorn Thordarson <sveinbjornt@gmail.com>
2011-09-13 00:43:07 +0000

    Adding cat2html, adding GPL license

--- ManDrake/main.m
+++ ManDrake/main.m
@@ -1,10 +1,23 @@
-//
-//  main.m
-//  ManDrake
-//
-//  Created by Sveinbjorn Thordarson on 9/12/11.
-//  Copyright 2011 __MyCompanyName__. All rights reserved.
-//
+/*
+ 
+ ManDrake - Native open-source Mac OS X man page editor 
+ Copyright (C) 2011 Sveinbjorn Thordarson <sveinbjornt@gmail.com>
+ 
+ This program is free software; you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation; either version 2 of the License, or
+ (at your option) any later version.
+ 
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+ 
+ You should have received a copy of the GNU General Public License
+ along with this program; if not, write to the Free Software
+ Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ 
+ */
 
 #import <Cocoa/Cocoa.h>
 
